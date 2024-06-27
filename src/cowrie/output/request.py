e9b145fcb112202161445258d825f09647c50212
Lovisa Ivarsson <lovisaivarsson01@gmail.com>
2024-06-27 13:14:58 +0200

    added output with a request to  a database

---
+++ src/cowrie/output/request.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+import requests
+
+import cowrie.core.output
+from cowrie.core.config import CowrieConfig
+
+
+class Output(cowrie.core.output.Output):
+    """
+    request output
+    """
+
+    def start(self):
+        self.addr = CowrieConfig.get("output_request", "address")
+
+        print("Starting plugin to send logs to " + self.addr)
+
+    def stop(self):
+        print("Stopping plugin to request to " + self.addr)
+
+    def write(self, event):
+        if event["eventid"] == "cowrie.command.input":
+            response = requests.post(self.addr, json={
+                "src_ip": event["src_ip"],
+                "time_stamp": event["timestamp"],
+                "input_cmd": event["input"],
+                "honeypotname": "cowrie-honeypot",
+                "session_id": event["session"]
+            })
+        
+
+
+
+        
