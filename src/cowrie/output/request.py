82ee29721591c6cb5eff3051487c01aa025b3a9b
Lovisa Ivarsson <lovisaivarsson01@gmail.com>
2024-07-19 11:27:18 -0500

    added new database

--- src/cowrie/output/request.py
+++ src/cowrie/output/request.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 import requests
 import os
+import json
 
 import cowrie.core.output
 from cowrie.core.config import CowrieConfig
-
 
 class Output(cowrie.core.output.Output):
     """
@@ -12,7 +12,9 @@ class Output(cowrie.core.output.Output):
     """
 
     def start(self):
-        self.addr = CowrieConfig.get("output_request", "address")
+        self.DATABASE_URL = os.getenv("COLLECTION_URL_COWRIE")
+        self.API_KEY = os.getenv("MONGO_API_KEY_COWRIE")
+        self.HONEYPOT_NAME = os.getenv("HONEYPOT_NAME", "cowrie")
 
         print("Starting plugin to send logs to " + self.addr)
 
@@ -20,14 +22,39 @@ class Output(cowrie.core.output.Output):
         print("Stopping plugin to request to " + self.addr)
 
     def write(self, event):
+        if API_KEY is None:
+            print("API_KEY is not set")
+            return
+        if DATABASE_URL is None:
+            print("DATABASE_URL is not set")
+            return
+
         if event["eventid"] == "cowrie.command.input":
-            response = requests.post(self.addr, json={
-                "src_ip": event["src_ip"],
-                "time_stamp": event["timestamp"],
-                "input_cmd": event["input"],
-                "honeypot_name": os.getenv("HONEYPOT_NAME", "cowrie"),
-                "session_id": event["session"]
-            })
+            headers = {
+                'Content-Type': 'application/json',
+                'Access-Control-Request-Headers': '*',
+                'api-key': self.API_KEY,
+            }
+            _payload = {
+                "collection": "CowrieLogs",
+                "database": "CowrieLogs",
+                "dataSource": "CowrieLogs",
+                "document": {
+                    "session_id": event["session"],
+                    "src_ip": event["src_ip"],
+                    "time_stamp": event["timestamp"],
+                    "honeypot_name": self.HONEYPOT_NAME,
+                    "command": event["input"],
+                    "response": "",
+                    "isAnalyzed": False,
+                }
+            }
+            response = requests.post(self.DATABASE_URL, headers=headers, data=json.dumps(_payload))
+            if not response.ok:
+                print("Failed to send data to " + self.DATABASE_URL)
+                print(response.text)
+            else:
+                print("Sending CMD: " + event['input'] + " to " + self.DATABASE_URL)
         
 
 
